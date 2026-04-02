5e4c42178bc85940dc2bd505efaeb1cb0d94601d
けんと｜貯金箱 <shinboutakoyaki@gmail.com>
2026-04-03 07:57:33 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
-const CACHE_NAME = 'chokinkako-v2';
+const CACHE_NAME = 'chokinkako-v3';
 
-// インストール時（即座に有効化）
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-// 古いキャッシュを削除
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -17,7 +15,6 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// ネットワーク優先・失敗時にキャッシュ使用
 self.addEventListener('fetch', event => {
   const url = event.request.url;
   if (url.includes('yahoo') ||
@@ -36,7 +33,7 @@ self.addEventListener('fetch', event => {
       return response;
     }).catch(() => {
       return caches.match(event.request).then(cached => {
-        return cached || caches.match('./index.html');
+        return cached || caches.match('./index-5.html');
       });
     })
   );
